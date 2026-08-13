2adf54cbe9ff0cc30c05378ba479235308fec650
jayavikash13022007 <jayavikash130220073@gmail.com>
2026-08-13 09:46:38 +0530

    Java

--- Classwork/Exception2.java
+++ Classwork/Exception2.java
@@ -1,11 +1,16 @@
 package Classwork;
+class InsufficientBankBalance extends Exception{
+    InsufficientBankBalance(String s){
+        super(s);
+    }
+}
 class Bank{
     private int balance = 0;
     void DisplayBalance(){System.out.println("Balance = "+balance);}
     void Deposit(int amount){System.out.println("Amount deposited: "+amount);balance += amount;}
     void Withdraw(int amount){
         try{
-            if(amount > balance) throw new Exception("Insufficient Balance");
+            if(amount > balance) throw new InsufficientBankBalance("Insufficient Balance");
             else{System.out.println("Amount withdrawn: "+amount);balance -= amount;}
         }
         catch(Exception e){System.out.println(e);}
---
+++ Classwork/Exception5.java
@@ -0,0 +1,27 @@
+package Classwork;
+class InvalidMarkException extends Exception{
+    InvalidMarkException(String s){
+        super(s);
+    }
+}
+class Student{
+    private int mark;
+    void GetMark(int mark) throws InvalidMarkException{
+        try{
+            if(mark < 0 || mark > 100) throw new InvalidMarkException("Invalid Mark");
+            this.mark = mark;
+        }
+        catch(InvalidMarkException e){
+            throw e;
+        }
+    }
+}
+public class Exception5 {
+    public static void main(String args[]){
+        Student s = new Student();
+        try{
+            s.GetMark(120);
+        }
+        catch(Exception e){System.out.println(e);}
+    }
+}
